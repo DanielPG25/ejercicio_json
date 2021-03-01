918697c2baa6095c63b6bae1f95481aeea2e6ab1
Daniel Parrales García <dannyparrales16@gmail.com>
2021-03-01 13:32:52 +0100

    Añadida la función del menu

--- funciones_pokedex.py
+++ funciones_pokedex.py
@@ -9,3 +9,12 @@ def leer_json(fichero):
 	except:
 		print("Error al leer el fichero")
 		sys.exit(0)
+
+def Menu():
+	print("1. Estadística de un equipo")
+	print("2. Nombres de equipos")
+	print("3. Clasificación de la liga")
+	print("4. Quiniela por fecha")
+	print("5. Salir")
+	opcion = int(input("Dime que opción eliges: "))
+	return opcion
