53f5612c74142e1a28a41a6b05703cdad80005be
Daniel Parrales García <dannyparrales16@gmail.com>
2021-03-01 15:48:35 +0100

    Añadida la función para calcular el ranking

--- funciones_pokedex.py
+++ funciones_pokedex.py
@@ -28,3 +28,81 @@ def SumarEstadisticas(fichero):
 		nombrebase["stats"] = (a.get("base").get("HP")) + (a.get("base").get("Attack")) + (a.get("base").get("Defense")) + (a.get("base").get("Sp. Attack")) + (a.get("base").get("Sp. Defense")) + (a.get("base").get("Speed"))
 		lista.append(nombrebase)
 	return lista	
+
+def Ranking(fichero):
+	lista=SumarEstadisticas(fichero)
+	primeroe = 0
+	segundoe = 0
+	terceroe = 0
+	cuartoe = 0
+	quintoe = 0
+	sextoe = 0
+	septimoe = 0
+	octavoe = 0
+	novenoe = 0
+	decimoe = 0
+	primeron = "a"
+	segundon = "b"
+	terceron = "c"
+	cuarton = "d"
+	quinton = "e"
+	sexton = "f"
+	septimon = "g"
+	octavon = "h"
+	novenon = "i"
+	decimon = "j"
+	for posicion in lista:
+		if posicion.get("stats") > primeroe:
+			segundoe = primeroe
+			primeroe = posicion.get("stats")  
+			segundon = primeron
+			primeron = posicion.get("nombre")
+		elif posicion.get("stats") > segundoe and posicion.get("stats") <= primeroe:
+			terceroe = segundoe
+			segundoe = posicion.get("stats")  
+			terceron = segundon
+			segundon = posicion.get("nombre")
+		elif posicion.get("stats") > terceroe and posicion.get("stats") <= segundoe:
+			cuartoe = terceroe
+			terceroe = posicion.get("stats")  
+			cuarton = terceron
+			terceron = posicion.get("nombre")
+		elif posicion.get("stats") > cuartoe and posicion.get("stats") <= terceroe:
+			quintoe = cuartoe
+			cuartoe = posicion.get("stats")  
+			quinton = cuarton
+			cuarton = posicion.get("nombre")
+		elif posicion.get("stats") >	quintoe and posicion.get("stats") <= cuartoe:
+			sextoe = quintoe
+			quintoe = posicion.get("stats")
+			sexton = quinton
+			quinton = posicion.get("nombre")
+		elif posicion.get("stats") > sextoe and posicion.get("stats") <= quintoe:
+			septimoe = sextoe
+			sextoe = posicion.get("stats")
+			septimon = sexton
+			sexton = posicion.get("nombre")
+		elif posicion.get("stats") > septimoe and posicion.get("stats") <= sextoe:
+			octavoe = septimoe
+			septimoe = posicion.get("stats")
+			octavon = septimon
+			septimon = posicion.get("nombre")
+		elif posicion.get("stats") > octavoe and posicion.get("stats") <= septimoe:
+			novenoe = octavoe
+			octavoe = posicion.get("stats")
+			novenon = octavon
+			octavon = posicion.get("nombre")
+		elif posicion.get("stats") > novenoe and posicion.get("stats") <= octavoe:
+			decimoe = novenoe
+			novenoe = posicion.get("stats")
+			decimon = novenon
+			novenon = posicion.get("nombre")
+		elif posicion.get("stats") > decimoe and posicion.get("stats") <= novenoe:
+			decimoe = posicion.get("stats")
+			decimon = posicion.get("nombre")
+	nombres=[]
+	estadisticas=[]
+	nombres.extend([primeron,segundon,terceron,cuarton,quinton,sexton,septimon,octavon,novenon,decimon])
+	estadisticas.extend([primeroe,segundoe,terceroe,cuartoe,quintoe,sextoe,septimoe,octavoe,novenoe,decimoe])
+	return nombres,estadisticas								
+
